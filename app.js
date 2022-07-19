b3cd62fba28436011a388daeabe5ddf2fffbbe1f
DmitriyTsay <106547568+DmitriyTsay@users.noreply.github.com>
2022-07-19 20:50:55 +0600

    v1.0 first working correctly solution, need to improve, redesign and etc

--- app.js
+++ app.js
@@ -1,49 +1,58 @@
-
-const hoursBlock = document.querySelectorAll('.hours');
-const minutesBlock = document.querySelectorAll('.minutes');
-const secondsBlock = document.querySelectorAll('.seconds');
-const submitButton = document.querySelector('.submit-button');
-const input = document.querySelector('#date');
-const topClones = document.querySelectorAll('.top-clone');
-const bottomClones = document.querySelectorAll('.bottom-clone');
-
-// days vars
+// Getting elements
+// days
 const days1 = document.querySelectorAll('.days-1');
 const days2 = document.querySelectorAll('.days-2');
 const days3 = document.querySelectorAll('.days-3');
-
-// hours vars
+// hours
 const hours1 = document.querySelectorAll('.hours-1');
 const hours2 = document.querySelectorAll('.hours-2');
-
-// minutes vars
+// minutes
 const minutes1 = document.querySelectorAll('.minutes-1');
 const minutes2 = document.querySelectorAll('.minutes-2');
-
-// seconds vars
+// seconds
 const seconds1 = document.querySelectorAll('.seconds-1');
 const seconds2 = document.querySelectorAll('.seconds-2');
+// button
+const submitButton = document.querySelector('.submit-button');
+const input = document.querySelector('#date');
 
+/* Algorithm for calculating countdown:
+    1. Setting the finishdate (initial, or from input)
+    2. Calculate the difference between current date and finishdate
+    3. Reduce difference by 1 second in 1000ms
+*/
+
+/* Algorithm for animation:
+    
+*/
+
+// Step 1.1 Setting finishDate (initial)
 let finishDate = new Date();
 
-finishDate.setFullYear(2022, 7, 15);
+finishDate.setFullYear(2023, 0, 1);
 finishDate.setHours(0,0,0,0);
 
-function turnOffAnimation() {
-    topClones.forEach((el) => {
-        el.classList.remove('top-animation');
-    })
+// Step 1.2 Setting finishDate (from input)
 
-    bottomClones.forEach((el) => {
-        el.classList.remove('bottom-animation');
-    })
-}
+submitButton.addEventListener('click', () => {
+    const inputValue = input.value;
+    
+    if (inputValue.length === 0) { // If input is not valid or empty setting next New Year Date
+        finishDate.setFullYear(2023, 0, 1);
+        finishDate.setHours(0,0,0,0);
+    }
+    else { // If input is valid parse it to finishDate var
+        finishDate = (new Date(Date.parse(inputValue)));
+    }
+});
+
+// Step 2. Calculate the difference
 
 function calculateDifference() {
     const currentDate = new Date();
 
     const difference = finishDate.getTime() - currentDate.getTime();
-
+    
     const days = Math.floor(difference / (86400 * 1000));
     const hours = Math.floor(difference / (3600 * 1000) % 24);
     const minutes = Math.floor(difference / (60 * 1000) % 60);
@@ -52,145 +61,72 @@ function calculateDifference() {
     return [days, hours, minutes, seconds];
 }
 
-function renderTimer(array) {
-
-    const daysArray = array[0].toString().split('');
-    const hoursArray = array[1].toString().split('');
-    const minutesArray = array[2].toString().split('');
-    const secondsArray = array[3].toString().split('');
-
-    // days render
-    if (daysArray.length === 3) {
-        days1.forEach((element) => {
-            element.innerHTML = daysArray[0];
-        });
-
-        days2.forEach((element) => {
-            element.innerHTML = daysArray[1];
-        });
-
-        days3.forEach((element) => {
-            element.innerHTML = daysArray[2];
-        });
-    } else if (daysArray.length === 2) {
-        days1.forEach((element) => {
-            element.innerHTML = 0;
-        });
-
-        days2.forEach((element) => {
-            element.innerHTML = daysArray[0];
-        });
-
-        days3.forEach((element) => {
-            element.innerHTML = daysArray[1];
-        });
-    } else if (daysArray.length === 1) {
-        days1.forEach((element) => {
-            element.innerHTML = 0;
-        });
-
-        days2.forEach((element) => {
-            element.innerHTML = 0;
-        });
-
-        days3.forEach((element) => {
-            element.innerHTML = daysArray[0];
-        });
-    }
-
-    // hours render 
-    if (hoursArray.length === 2) {
-        hours1.forEach((element) => {
-            element.innerHTML = hoursArray[0];
-        });
-
-        hours2.forEach((element) => {
-            element.innerHTML = hoursArray[1];
-        });
-    } else if (hoursArray.length === 1) {
-        hours1.forEach((element) => {
-            element.innerHTML = 0;
-        });
-
-        hours2.forEach((element) => {
-            element.innerHTML = hoursArray[0];
-        });
-    } 
-
-    // minutes render
-    if (minutesArray.length === 2) {
-        minutes1.forEach((element) => {
-            element.innerHTML = minutesArray[0];
-        });
-
-        minutes2.forEach((element) => {
-            element.innerHTML = minutesArray[1];
-        });
-    } else if (minutesArray.length === 1) {
-        minutes1.forEach((element) => {
-            element.innerHTML = 0;
-        });
-
-        minutes2.forEach((element) => {
-            element.innerHTML = minutesArray[0];
-        });
-    } 
-
-    // seconds render
-    if (secondsArray.length === 2) {
-        seconds1.forEach((element) => {
-            element.innerHTML = secondsArray[0];
-        });
-
-        seconds2.forEach((element) => {
-            element.innerHTML = secondsArray[1];
-        });
-    } else if (secondsArray.length === 1) {
-        seconds1.forEach((element) => {
-            element.innerHTML = 0;
-        });
-
-        seconds2.forEach((element) => {
-            element.innerHTML = secondsArray[0];
-        });
-    } 
-}
-
-function combo() {
-    renderTimer(calculateDifference());
-}
-
-const showTime = () => {
+function flipBlock(elementList, value = 0) {
+    if (sameValue(elementList, value) === false) {
+        elementList[2].classList.add('top-animation');
+        elementList[3].classList.add('bottom-animation');
+        
+        setTimeout(() => {
+            elementList.forEach((element) => {
+                element.innerHTML = value;
+            })
+        }, 250);
     
-    topClones.forEach((el) => {
-        el.classList.add('top-animation');
-    })
-
-    bottomClones.forEach((el) => {
-        el.classList.add('bottom-animation');
-    })
-
-    setTimeout(turnOffAnimation,500);
-    // console.log(`Текущая дата: ${currentDate}`);
-    // console.log(`Конечная дата: ${finishDate}`);
-
-    setTimeout(combo, 275);
-
+        setTimeout(() => {
+            elementList[2].classList.remove('top-animation');
+            elementList[3].classList.remove('bottom-animation');
+        }, 500);
+    }
 }
 
-showTime();
-setInterval(showTime,1000);
+function sameValue(elementList, value) {
+    if (elementList[0].innerHTML == value) {
+        return true;
+    }
 
-submitButton.addEventListener('click', () => {
-    const inputValue = input.value;
+    return false;
+}
+
+function renderTimer() {
+    let [days, hours, minutes, seconds] = calculateDifference();
     
-    // console.log(inputValue);
+    if (days.toString().length === 3) {
+        flipBlock(days1, days.toString()[0]);
+        flipBlock(days2, days.toString()[1]);
+        flipBlock(days3, days.toString()[2]);
+    } else if (days.toString().length === 2) {
+        flipBlock(days1, 0);
+        flipBlock(days2, days.toString()[0]);
+        flipBlock(days3, days.toString()[1])
+    } else if (days.toString().length === 1) {
+        flipBlock(days1, 0);
+        flipBlock(days2, 0);
+        flipBlock(days3, days.toString()[0]);
+    }
 
-    if (inputValue.length === 0) {
-        finishDate.setFullYear(2023, 0, 1);
-        finishDate.setHours(0,0,0,0);
+    if (hours.toString().length === 2) {
+        flipBlock(hours1, hours.toString()[0]);
+        flipBlock(hours2, hours.toString()[1]);
+    } else if (hours.toString().length === 1) {
+        flipBlock(hours1);
+        flipBlock(hours2, hours.toString()[0]);
     }
-    else {
-        finishDate = (new Date(Date.parse(inputValue)));
+
+    if (minutes.toString().length === 2) {
+        flipBlock(minutes1, minutes.toString()[0]);
+        flipBlock(minutes2, minutes.toString()[1]);
+    } else if (minutes.toString().length === 1) {
+        flipBlock(minutes1);
+        flipBlock(minutes2, minutes.toString()[0]);
     }
-});
+
+    if (seconds.toString().length === 2) {
+        flipBlock(seconds1, seconds.toString()[0]);
+        flipBlock(seconds2, seconds.toString()[1]);
+    } else if (seconds.toString().length === 1) {
+        flipBlock(seconds1);
+        flipBlock(seconds2, seconds.toString()[0]);
+    }
+}
+
+setInterval(renderTimer, 1000);
